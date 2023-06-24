31f03df9d0574e119b61036d8f112fcf674d8859
cornea4627 <2g4mpmxc@duck.com>
2023-06-24 13:10:07 -0700

    s

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ const player = {
     x: 50,
     y: canvas.height - 50,
     width: 30,
-    height: 30,
+    height: 70,
     color: 'blue',
-    dx: 5
-}
+    dx: 5,
+    dy: 0,
+    gravity: 0.1,
+    jumpStrength: 10,
+    onGround: true
+};
 gameObjects.push(player);
 
 // listen for keyboard input
@@ -20,6 +24,9 @@ document.addEventListener('keydown', (e) => {
         player.x += player.dx;
     } else if (e.key === 'ArrowLeft') {
         player.x -= player.dx;
+    } else if (e.key === ' ' && player.onGround) { // Space bar for jump
+        player.dy =- player.jumpStrength;
+        player.onGround = false;
     }
 });
 
@@ -29,10 +36,20 @@ function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     // Update game objects
+    player.dy += player.gravity; // Apply gravity
+    player.y += player.dy;
 
+    // check if player is on the ground
+    if (player.y >= canvas.height - player.height) {
+        player.y = canvas.height - player.height;
+        player.dy = 0;
+        player.onGround = true;
+    }
+    
     // Render game objects
     ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.width, player.height);
+
     // Request the next animation frame
     requestAnimationFrame(gameLoop);
 }
